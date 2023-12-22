11ef8214ac877b1981556ce0e6846ebe703ef1e6
Vladislav Tarasov <tarvlad@inbox.ru>
2023-12-22 19:50:22 +0700

    Cosmetic changes

--- src/interrupts_handlers.asm
+++ src/interrupts_handlers.asm
@@ -8,13 +8,20 @@ extern send_command_8259_master
 extern printfmt
 
 msg:
-db "t i c k__tock timer",0
+db `__timer_tick__[%x]\n`,0
+
+ctr:
+dd 0
 
 ; void timer_interrupt_handler()
 timer_interrupt_handler:
+push dword [ctr]
 push msg
 call printfmt
-add esp, 4
+add esp, 8
+mov eax, [ctr]
+inc eax
+mov [ctr], eax
 push 0x20                   ; end of interrupt 
 call send_command_8259_master
 add esp, 4
