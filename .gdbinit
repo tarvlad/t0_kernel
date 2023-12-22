feb8e0a667021c5a684b377b47e32f163ed37d1a
Vladislav Tarasov <tarvlad@inbox.ru>
2023-12-22 15:45:12 +0700

    Cleanup

--- .gdbinit
+++
@@ -1,6 +0,0 @@
-target remote localhost:1234
-set arch i8086
-set disassembly-flavor intel
-break *0x7c00
-c
-x/16i $cs*16+$eip
--- idt_setup.asm
+++
@@ -1,9 +0,0 @@
-[bits 32]
-
-global __idt_load
-
-; void  __idt_load(u4 idt_descriptor);
-__idt_load:
-mov eax, [esp + 4]
-lidt [eax]
-ret
